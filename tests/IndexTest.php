96e294476b4fa06e2fe52858250eb17d2dcba00b
n3wborn <n3wborn@protonmail.com>
2022-03-28 23:42:16 +0200

    test: add basic test

---
+++ tests/IndexTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testHello(): void
+    {
+        $_GET['name'] = 'n3wborn';
+
+        ob_start();
+        include 'src/public/index.php';
+        $content = ob_get_clean();
+
+        $this->assertEquals('Hello n3wborn', $content);
+    }
+}

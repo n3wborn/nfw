6fa543b6cc8b702cf4e168287c08bcc8e0346651
n3wborn <n3wborn@protonmail.com>
2022-04-03 21:45:01 +0200

    refac(index): replace route map by Routing component

--- src/public/index.php
+++ src/public/index.php
@@ -6,6 +6,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
 /*
  * contrary to the way it's done in the doc
@@ -14,21 +15,21 @@ use Symfony\Component\HttpFoundation\Response;
  */
 
 $request = Request::createFromGlobals();
+$routes = include __DIR__.'/../../src/app.php';
 
-$map = [
-    '/hello' => 'hello',
-    '/bye' => 'bye',
-];
+$context = (new Routing\RequestContext())->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../pages/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../../src/pages/%s.php', $_route);
+
     $response = new Response(ob_get_clean());
-} else {
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();

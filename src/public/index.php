ad7897c45e114d6946b2782eb463c3f823045967
n3wborn <n3wborn@protonmail.com>
2022-04-06 11:54:28 +0200

    feat(controller): update response depending on _controller attribute

--- src/public/index.php
+++ src/public/index.php
@@ -30,11 +30,8 @@ $context = (new Routing\RequestContext())->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {

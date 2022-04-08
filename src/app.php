c146712bf90cf7f293650dcc6a851b9d0fbf78cf
n3wborn <n3wborn@protonmail.com>
2022-04-08 09:19:32 +0200

    feat: add is_leap_year route and function

--- src/app.php
+++ src/app.php
@@ -3,11 +3,34 @@
 declare(strict_types=1);
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
-$routes = new Routing\RouteCollection();
+/** @param int|null $year */
+function is_leap_year($year = null): bool
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
 
-$routes->add('hello', new Routing\Route('/hello/{name}', [
+    return 0 === $year % 400 || (0 === $year % 4 && 0 != $year % 100);
+}
+
+$routes = new RouteCollection();
+
+$routes->add('is_leap_year', new Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function (Request $request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yeap, this is a leap year');
+        }
+
+        return new Response('Nope, this is not a leap year');
+    },
+]));
+
+$routes->add('hello', new Route('/hello/{name}', [
     'name' => 'World',
     '_controller' => function (Request $request) {
         $response = render_template($request);
@@ -18,7 +41,7 @@ $routes->add('hello', new Routing\Route('/hello/{name}', [
     },
 ]));
 
-$routes->add('bye', new Routing\Route('/bye', [
+$routes->add('bye', new Route('/bye', [
     '_controller' => function (Request $request) {
         $response = render_template($request);
 

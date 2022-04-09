4713b8f5894835ff5ec4743f8150d55ace705c30
Stéphane P <n3wborn@protonmail.com>
2022-04-09 19:44:23 +0200

    feat: templating (#5)
    * feat: create render_template function
    
    * feat(controller): update response depending on _controller attribute
    
    * refac(app): add a function for _controller attribute
    
    * refac: add type hinting
    
    * feat: add is_leap_year route and function
    
    * refac(app): use >=php7 features, better type hints

--- src/app.php
+++ src/app.php
@@ -2,10 +2,50 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
-$routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Routing\Route('/bye'));
+function is_leap_year(?string $year = null): bool
+{
+    $year ??= intval(date('Y'));
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes = new RouteCollection();
+
+$routes->add('is_leap_year', new Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function (Request $request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yeap, this is a leap year');
+        }
+
+        return new Response('Nope, this is not a leap year');
+    },
+]));
+
+$routes->add('hello', new Route('/hello/{name}', [
+    'name' => 'World',
+    '_controller' => function (Request $request) {
+        $response = render_template($request);
+
+        $response->headers->set('Content-type', 'text/plain');
+
+        return $response;
+    },
+]));
+
+$routes->add('bye', new Route('/bye', [
+    '_controller' => function (Request $request) {
+        $response = render_template($request);
+
+        $response->headers->set('Content-type', 'text/plain');
+
+        return $response;
+    },
+]));
 
 return $routes;

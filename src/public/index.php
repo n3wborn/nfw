559bc5e820c48f2f26f482557a6d84d22cb9e784
Stéphane P <n3wborn@protonmail.com>
2022-04-09 19:44:23 +0200

    feat: templating (#5)
    * feat: create render_template function
    
    * feat(controller): update response depending on _controller attribute
    
    * refac(app): add a function for _controller attribute
    
    * refac: add type hinting
    
    * feat: add is_leap_year route and function
    
    * refac(app): use >=php7 features, better type hints

--- src/public/index.php
+++ src/public/index.php
@@ -14,6 +14,15 @@ use Symfony\Component\Routing;
  * we use index.php as the front controller
  */
 
+function render_template(Request $request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../../src/app.php';
 
@@ -21,11 +30,8 @@ $context = (new Routing\RequestContext())->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {

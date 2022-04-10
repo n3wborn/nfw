56f7d9fb61012d47306e592f207c4851d7f1c5ee
n3wborn <n3wborn@protonmail.com>
2022-04-10 19:03:50 +0200

    refac(LeapYearController): set class as final, add index return type

--- src/Controller/LeapYearController.php
+++ src/Controller/LeapYearController.php
@@ -7,9 +7,9 @@ namespace Nfw\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class LeapYearController
+final class LeapYearController
 {
-    public function index(Request $request)
+    public function index(Request $request): Response
     {
         if (is_leap_year($request->attributes->get('year'))) {
             return new Response('Yeap, this is a leap year');

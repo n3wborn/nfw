f254b613cb582f65b1245c40950d6a90b3daaaf6
Stéphane P <n3wborn@protonmail.com>
2022-04-15 09:52:53 +0200

    feat: separation of concerns (#7)
    * feat: create RouteController
    
    * feat: create Framework class
    
    * refac(index): use RouteController and Framework
    
    * refac: move is_leap_year() inside LeapYearController
    
    * refac: remove app.php
    
    * refac: move src/Framework to src/Framework/Framework
    
    * refac: move LeapYearController to src/Calendar/Controller/LeapYearController
    
    * feat: create LeapYear class in src/Calendar/Model
    
    * refac(RouteController): update is_leap_year _controller value
    
    * refac(index): update use statement

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+use Nfw\Controller\RouteController;
+use Nfw\Framework\Framework;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
@@ -26,7 +28,7 @@ function render_template(Request $request): Response
 }
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../../src/app.php';
+$routes = (new RouteController())->route();
 
 $context = (new Routing\RequestContext())->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
@@ -34,17 +36,7 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();

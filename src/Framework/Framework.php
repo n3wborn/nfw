c4c62b5c0c0906418e97230d05e3f41c22bc98a3
n3wborn <n3wborn@protonmail.com>
2022-04-15 10:18:58 +0200

    refac(Framework): replace each HttpKernel/Routing class by their interface

--- src/Framework/Framework.php
+++ src/Framework/Framework.php
@@ -7,17 +7,17 @@ namespace Nfw\Framework;
 use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 final class Framework
 {
     public function __construct(
-        private UrlMatcher $matcher,
-        private ControllerResolver $controllerResolver,
-        private ArgumentResolver $argumentResolver
+        private UrlMatcherInterface $matcher,
+        private ControllerResolverInterface $controllerResolver,
+        private ArgumentResolverInterface $argumentResolver
     ) {
     }
 

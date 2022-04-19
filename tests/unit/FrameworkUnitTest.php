e6a5335e56044a31164ca56eac51f23ef9e16f25
Stéphane P <n3wborn@protonmail.com>
2022-04-19 09:18:10 +0200

    tests: unit tests (#8)
    * chore(deps): update phpunit
    
    * chore(phpunit): create config file
    
    * refac(Framework): replace each HttpKernel/Routing class by their interface
    
    * fix(Framework): set Not Found Response to 404
    
    * chore: update tests namespace
    
    * chore(git): ignore phpunit cache file
    
    * tests: remove IndexTest
    
    * tests: add FrameworkUnitTest
    
    * tests: add testErrorHandling
    
    * tests: add LeapYearController tests
    
    * tests: refac LeapYearController tests using a dataprovider
    
    * refac(tests): add missing type hints

---
+++ tests/unit/FrameworkUnitTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Exception;
+use Generator;
+use Nfw\Calendar\Controller\LeapYearController;
+use Nfw\Framework\Framework;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Routing\RequestContext;
+
+class FrameworkUnitTest extends TestCase
+{
+    public function testNotFoundHandling(): void
+    {
+        $framework = $this->getFrameworkForException(new ResourceNotFoundException());
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testErrorHandling(): void
+    {
+        $framework = $this->getFrameworkForException(new RuntimeException());
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(500, $response->getStatusCode());
+    }
+
+    public function leapYearGenerator(): Generator
+    {
+        yield [
+            '1980',
+            'Yeap, this is a leap year',
+        ];
+
+        yield [
+            '1981',
+            'Nope, this is not a leap year',
+        ];
+    }
+
+    /**
+     * @dataProvider leapYearGenerator
+     */
+    public function testLeapControllerResponse(string $year, string $answer): void
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->returnValue([
+                '_route' => 'is_leap_year/${year}',
+                'year' => $year,
+                '_controller' => [new LeapYearController(), 'index'],
+            ]))
+        ;
+
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(RequestContext::class)))
+        ;
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString($answer, $response->getContent());
+    }
+
+    private function getFrameworkForException(Exception $exception): Framework
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->throwException($exception))
+        ;
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(RequestContext::class)))
+        ;
+        $controllerResolver = $this->createMock(ControllerResolverInterface::class);
+        $argumentResolver = $this->createMock(ArgumentResolverInterface::class);
+
+        return new Framework($matcher, $controllerResolver, $argumentResolver);
+    }
+}

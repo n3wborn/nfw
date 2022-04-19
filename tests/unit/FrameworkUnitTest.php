4d1e5059550838f948e7ab4c7052f256bfe8972f
n3wborn <n3wborn@protonmail.com>
2022-04-19 08:56:16 +0200

    tests: add LeapYearController tests

--- tests/unit/FrameworkUnitTest.php
+++ tests/unit/FrameworkUnitTest.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Nfw\Calendar\Controller\LeapYearController;
 use Nfw\Framework\Framework;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
@@ -32,6 +35,68 @@ class FrameworkUnitTest extends TestCase
         $response = $framework->handle(new Request());
 
         $this->assertEquals(500, $response->getStatusCode());
+    }
+
+    public function testLeapControllerLeapYearResponse()
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->returnValue([
+                '_route' => 'is_leap_year/${year}',
+                'year' => '1980',
+                '_controller' => [new LeapYearController(), 'index'],
+            ]))
+        ;
+
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(RequestContext::class)))
+        ;
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Yeap, this is a leap year', $response->getContent());
+    }
+
+    public function testLeapControllerNoLeapYearResponse()
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->returnValue([
+                '_route' => 'is_leap_year/${year}',
+                'year' => '1981',
+                '_controller' => [new LeapYearController(), 'index'],
+            ]))
+        ;
+
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(RequestContext::class)))
+        ;
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Nope, this is not a leap year', $response->getContent());
     }
 
     private function getFrameworkForException($exception)
